08fea0d2a2bf4065348067a67000abcce8e06238
Илья Егоров <Shepherd_MacBook@Ilya-2.local>
2020-09-16 23:40:05 +0300

    сделал верстку таймкодов

--- PodcastEdit/PodcastEditViewController.swift
+++ PodcastEdit/PodcastEditViewController.swift
@@ -8,10 +8,16 @@
 
 import UIKit
 
+
 final class PodcastEditViewController: UIViewController {
     
     @IBOutlet private weak var stackView: UIStackView!
+//    @IBOutlet private weak var audioPlot: AKOutputWaveformPlot!
     
+//    var viewa: AKplot
+
+    
+    private var isPlaying = false
     private var timeCodes = [Int]()
     
     
@@ -19,8 +25,23 @@ final class PodcastEditViewController: UIViewController {
         navigationController?.popViewController(animated: true)
     }
     
+    @IBAction func didTapPlay(_ sender: UIButton) {
+        view.endEditing(true)
+
+        isPlaying.toggle()
+        let image = isPlaying ? #imageLiteral(resourceName: "pause_48") : #imageLiteral(resourceName: "icon_play")
+        sender.setImage(image, for: .normal)
+    }
+    
     @IBAction func didTapAddTimecode(_ sender: UIButton) {
         guard let index = stackView.arrangedSubviews.firstIndex(of: sender) else { return }
+        
+         guard let timeCodeView = Bundle.main
+            .loadNibNamed(String(describing: TimeCodeView.self), owner: self, options: nil)?
+            .compactMap({ $0 as? TimeCodeView })
+            .first else { return }
+        
+        stackView.insertArrangedSubview(timeCodeView, at: index)
     }
     
     @IBAction func didTapNext(_ sender: Any) {
--- Podfile
+++ Podfile
@@ -4,4 +4,5 @@ use_frameworks!
 target 'PodcastEdit' do
   pod 'SnapKit'
   pod 'YPImagePicker'
+  pod 'AudioKit', '~> 4.0'
 end
